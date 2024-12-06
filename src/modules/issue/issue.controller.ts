e1a727f0a432f12da2a582de6067a1f3a5b121f9
Alexandre GarciaPerez <you@example.com>
2024-12-06 10:51:51 +0100

    Controller Done

---
+++ src/modules/issue/issue.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Post, HttpCode, Body, ConflictException } from '@nestjs/common';
+import { Issueservice } from './issue.service';
+import { Issue } from '../../schemas/issue.schema';
+import { ValidationPipe } from '@nestjs/common';
+
+@Controller('issue')
+export class IssueController {
+    private readonly issueService: Issueservice;
+
+    public constructor(issueService: Issueservice) {
+        this.issueService = issueService;
+    }
+
+    @Get()
+    @HttpCode(200)
+    async getAllIssues(): Promise<Issue[]> {
+        return this.issueService.getAllIssues();
+    }
+
+    @Post('create-issue')
+    @HttpCode(201)
+    async createOneIssue(@Body(new ValidationPipe()) IssueDto: { issueName: string }): Promise<Issue> {
+        const existingIssue: Issue = await this.issueService.getIssueByName(IssueDto.issueName);
+        if (existingIssue) {
+            throw new ConflictException('Issue already exists');
+        }
+        return this.issueService.createIssue(IssueDto);
+    }
+}
