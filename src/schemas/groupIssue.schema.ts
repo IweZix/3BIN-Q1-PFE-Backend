947da6fdd2f18c7a4f143f342dd72cfa622d305a
Alexandre GarciaPerez <you@example.com>
2024-12-06 11:41:39 +0100

    init branch adn schema DOne

---
+++ src/schemas/groupIssue.schema.ts
@@ -0,0 +1,14 @@
+import { Schema, Prop, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+/**
+ * The group issue schema.
+ * Used by the issue module to access the group issue collection in the database.
+ */
+@Schema()
+export class GroupIssue extends Document {
+    @Prop({ required: true })
+    groupIssueName: string;
+}
+
+export const GroupIssueSchema = SchemaFactory.createForClass(GroupIssue);
