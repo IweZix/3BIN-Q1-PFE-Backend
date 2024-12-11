824420571e18b6dc17fac7e2868eb726920fb213
Karen Asipuela Paredes <karen.asipuela@student.vinci.be>
2024-12-11 18:46:01 +0100

    fix create issue

--- src/modules/issue/issue.service.ts
+++ src/modules/issue/issue.service.ts
@@ -15,9 +15,22 @@ export class IssueService {
     }
 
     public async createIssue(issueName: string, groupIssueName: string): Promise<Issue> {
-        const newIssue = new this.IssueModel({ issueName, group_name: groupIssueName });
+        // Récupérer le dernier ID
+        const lastIssue = await this.IssueModel.findOne().sort({ _id: -1 }).exec();
+    
+        // Calculer le nouvel ID
+        const newId = lastIssue ? parseInt(lastIssue._id, 10) + 1 : 1;
+    
+        // Créer une nouvelle issue avec le nouvel ID
+        const newIssue = new this.IssueModel({
+            _id: newId.toString(), // Convertir en chaîne car _id est un string
+            issueName,
+            group_name: groupIssueName
+        });
+    
         return newIssue.save();
     }
+    
 
     public async getIssueByName(issueName: string): Promise<Issue> {
         return await this.IssueModel.findOne({ issueName }).exec();
